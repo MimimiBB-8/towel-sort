d0e366c849820a51026d9daa0647fe69d10f8a0b
MimimiBB-8 <yliame14@gmail.com>
2021-03-12 22:02:48 +0300

    feat: implement logic

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ module.exports = function towelSort (matrix) {
   var result = []
   for (let i=0; i< matrix.length; i++){
     if (i%2 == 0){
-      result.push(...matrix[i])
+        result = result.concat(matrix[i])
     } else {  
-      result.push(...matrix[i].reverse())
+        result = result.concat(matrix[i].reverse())
     }
   }
-  console.log(result)
   return result;
 }
